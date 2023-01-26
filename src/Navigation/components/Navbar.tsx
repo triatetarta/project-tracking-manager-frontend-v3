d9f2672b727471ef38bbcb884838f870535a55bc
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2023-01-26 11:02:31 +0000

    debug login button

--- src/Navigation/components/Navbar.tsx
+++ src/Navigation/components/Navbar.tsx
@@ -85,6 +85,17 @@ const Navbar = ({
                   </div>
                 </Link>
               </div>
+              <div>
+                <button
+                  className='ml-2 z-50 bg-red-600 px-2 py-1 rounded-md text-white text-sm'
+                  onClick={() => {
+                    localStorage.setItem("persist", JSON.stringify(false));
+                    window.location.reload();
+                  }}
+                >
+                  DEBUG
+                </button>
+              </div>
             </>
           ) : null}
         </div>
